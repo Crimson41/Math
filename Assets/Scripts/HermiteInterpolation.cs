2b654f3233b97af51a30c848c72035b3bbfeb7d7
Crimson41 <111068020+Crimson41@users.noreply.github.com>
2024-04-27 14:39:14 +0200

    Initial commit

---
+++ Assets/Scripts/HermiteInterpolation.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+[RequireComponent(typeof(LineRenderer))]
+public class HermiteInterpolation : MonoBehaviour
+{
+    // Points graphiquement relevés :
+    // (Abscisses, ordonnées, tangentes)
+    [SerializeField] List<Vector3> _points = new()
+    {
+        new(2, 3, -10), // A
+        new(2, 7, 1), // B
+        new(6, 7, -1), // C
+        new(4, 9.5f, -0.1f), // D
+        new(8, 7, 2), // E
+        new(9, 4.5f, 0), // F
+        new(9.5f, 5.5f, -1), // G
+        new(6, 3, -0.5f), // H
+        new(6, 2, -0.3f), // I
+    };
+
+    private List<float> X_final; // Abscisses finaux
+    private List<float> Y_final; // Ordonnées finaux
+    private int _precisionPoints = 10;
+    private LineRenderer _lineRenderer;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _points.Add(new());
+        Draw();
+    }
+
+    public void Draw()
+    {
+        Debug.Log("Drawing new curves");
+        Vector3 lastPoint = _points.ElementAt(0);
+        lastPoint.z = 0;
+        _points[9] = lastPoint;
+
+        X_final = new(); // Abscisses finaux
+        Y_final = new(); // Ordonnées finaux
+
+        // Parcourir les points relevés et effectuer l'interpolation Hermite entre eux
+        for (int i = 0; i < _points.Count - 1; i++)
+        {
+            Vector3 p0 = _points[i];
+            Vector3 p1 = _points[i + 1];
+            Hermite(p0.x, p0.y, p0.z, p1.x, p1.y, p1.z, _precisionPoints);
+        }
+
+        DrawCurve(); // Rendu final
+    }
+
+    private void Hermite(float x0, float y0, float m0, float x1, float y1, float m1, int numPoints)
+    {
+        for (int i = 0; i < numPoints; i++)
+        {
+            float t = (float)i / (numPoints - 1);
+            float h00 = (1 + 2 * t) * Mathf.Pow(1 - t, 2);
+            float h10 = t * Mathf.Pow(1 - t, 2);
+            float h01 = Mathf.Pow(t, 2) * (3 - 2 * t);
+            float h11 = Mathf.Pow(t, 2) * (t - 1);
+            float X = h00 * x0 + h10 * (x0 + m0) + h01 * x1 + h11 * (x1 + m1);
+            float Y = h00 * y0 + h10 * (y0 + m0) + h01 * y1 + h11 * (y1 + m1);
+            X_final.Add(X);
+            Y_final.Add(Y);
+        }
+    }
+    public void DrawCurve()
+    {
+        //LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        _lineRenderer.positionCount = X_final.Count;
+        _lineRenderer.startWidth = 0.1f;
+        _lineRenderer.endWidth = 0.1f;
+        _lineRenderer.useWorldSpace = false;
+
+        for (int i = 0; i < X_final.Count; i++)
+        {
+            _lineRenderer.SetPosition(i, new Vector3(X_final[i], Y_final[i], 0));
+        }
+    }
+}
